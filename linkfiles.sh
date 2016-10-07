b4f77af9a35768bf104448019bd8abc7e008a155
Armin Briegel <ab@scriptingosx.com>
2016-10-07 10:03:29 +0200

    reworked linking with function

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,48 +1,49 @@
  #!/bin/bash
 
-PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/libexec export PATH
+PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/libexec:/usr/local/bin export PATH
 
-MY_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+# function to create a symlink
+symlink () { # srcdir destdir [srcItem] item
+    # even though the links point to the absolute path
+    if [ -e "$1/$3" ]; then
+        if [ ! -z "$4" ]; then
+            ln -sFi "$1/$3" "$2/$4"
+        else
+            ln -sFi "$1/$3" "$2/$3"
+        fi
+    else
+        echo "no file: $1/$3"
+    fi
+}
 
-ln -sFi "${MY_PATH}/inputrc" "${HOME}/.inputrc"
-ln -sFi "${MY_PATH}/bash_profile" "${HOME}/.bash_profile"
+# function to create a dir with mode
+createDir() { # path mode
+    if [ ! -d "$1" ]; then
+        mkdir -p "$1"
+        chmod "$2" "$1"
+    fi
+}
+
+
+MY_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+symlink "${MY_PATH}" "${HOME}" "inputrc" ".inputrc"
+symlink "${MY_PATH}" "${HOME}" "bash_profile" ".bash_profile"
 
 PROJECTS=$(dirname "$MY_PATH")
 echo "Projects Folder: $PROJECTS"
 
 BIN_FOLDER="${HOME}/bin"
+createDir "${BIN_FOLDER}" 750
 echo "bin folder: $BIN_FOLDER"
+
 # look for certain git projects in the Projects folder and link the executable to ~/bin
-
-if [[ -d "$PROJECTS/make-profile-pkg" ]]; then
-	ln -sFi "$PROJECTS/make-profile-pkg/make_profile_pkg.py" "$BIN_FOLDER/make_profile_pkg"
-fi
-
-if [[ -d "$PROJECTS/mcxToProfile" ]]; then
-	ln -sFi "$PROJECTS/mcxToProfile/mcxToProfile.py" "$BIN_FOLDER/mcxToProfile"
-fi
-
-if [[ -d "$PROJECTS/recipeutil" ]]; then
-	ln -sFi "$PROJECTS/recipeutil/recipeutil" "$BIN_FOLDER/recipeutil"
-fi
-
-if [[ -d "$PROJECTS/RemindersCLITool" ]]; then
-	ln -sFi "$PROJECTS/RemindersCLITool/reminders" "$BIN_FOLDER/reminders"
-fi
-
-if [[ -d "$PROJECTS/SoftwareCheck" ]]; then
-	ln -sFi "$PROJECTS/SoftwareCheck/softwarecheck" "$BIN_FOLDER/softwarecheck"
-fi
-
-if [[ -d "$PROJECTS/ThingsCLITool" ]]; then
-	ln -sFi "$PROJECTS/ThingsCLITool/things" "$BIN_FOLDER/things"
-fi
-
-if [[ -d "$PROJECTS/munki-pkg" ]]; then
-	ln -sFi "$PROJECTS/munki-pkg/munki-pkg" "$BIN_FOLDER/munki-pkg"
-fi
-
-if [[ -d "$PROJECTS/quickpkg" ]]; then
-	ln -sFi "$PROJECTS/quickpkg/quickpkg" "$BIN_FOLDER/quickpkg"
-fi
-
+symlink "$PROJECTS/make-profile-pkg" "${BIN_FOLDER}" "make_profile_pkg.py" "make_profile_pkg"
+symlink "$PROJECTS/mcxToProfile" "${BIN_FOLDER}" "mcxToProfile.py" "mcxToProfile"
+symlink "$PROJECTS/recipeutil" "${BIN_FOLDER}" "recipeutil"
+symlink "$PROJECTS/RemindersCLITool" "${BIN_FOLDER}" "reminders"
+symlink "$PROJECTS/SoftwareCheck" "${BIN_FOLDER}" "softwarecheck"
+symlink "$PROJECTS/thingsCLITool" "${BIN_FOLDER}" "things"
+symlink "$PROJECTS/munki-pkg" "${BIN_FOLDER}" "munkipkg"
+symlink "$PROJECTS/quickpkg" "${BIN_FOLDER}" "quickpkg"
+symlink "$PROJECTS/ssh-installer" "${BIN_FOLDER}" "ssh-installer.sh" "ssh-installer"
